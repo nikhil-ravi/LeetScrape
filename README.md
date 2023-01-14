d0df88187504482dee082ba9150cedbf1fe70964
Nikhil <nravi6@asu.edu>
2023-01-13 22:47:19 -0500

    Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,127 @@
-# Leetcode solutions
+# Leetcode Questions Scraper
+
+This package may be used to get the list of Leetcode questions and their topic and company tags, difficulty, question body (including test cases, constraints, hints), and code stubs in any of the available programming languages.
+
+## Usage
+
+---
+
+Import the relevant classes from the [`leetcode`](/src/leetcode/) package:
+
+```python
+from leetcode.GetQuestionsList import GetQuestionsList
+from leetcode.GetQuestionInfo import GetQuestionInfo
+import pandas as pd
+from tqdm import tqdm
+import pickle
+import numpy as np
+```
+
+Get the list of questions, companies, topic tags, categories using the [`GetQuestionsList`](/src/GetQuestionsList) class:
+
+```python
+ls = GetQuestionsList()
+ls.scrape()
+ls.questions["QID"] = ls.questions["QID"].astype(int)
+ls.to_csv(directory_path="../data/") # example directory path
+```
+
+Query individual question's information such as the body, test cases, constraints, hints, code stubs, and company tags using the [`GetQuestionInfo`](/src/leetcode/GetQuestionInfo.py) class:
+
+```python
+questions_info = pd.read_csv("../data/questions.csv")
+questions_info_list = []
+for i, (titleSlug, paidOnly) in enumerate(tqdm(questions_info[["titleSlug", "paidOnly"]].values)):
+    if not paidOnly:
+        questions_info_list.append(
+            GetQuestionInfo(titleSlug).scrape()
+        )
+
+with open("../data/questions_info_list.pickle", "wb") as f:
+    pickle.dump(questions_info_list, f)
+```
+
+The above code stub is time consuming (10+ minutes) since there are 2500+ questions.
+
+```python
+# with open(
+#     "../data/questionBody.pickle", "rb"
+# ) as f:
+    # questions_info_list = pickle.load(f)
+questions_body = pd.DataFrame(
+    questions_info_list
+).drop(columns=["titleSlug"])
+questions_body["QID"] = questions_body["QID"].astype(int)
+```
+
+Create a new dataframe with all the questions and their metadata and body information.
+
+```python
+questions = pd.concat(
+    [
+        ls.questions.set_index("QID"),
+        questions_body.set_index("QID")
+    ],
+    axis=1
+).reset_index()
+questions["Hints"] = questions["Hints"].fillna("").apply(list)
+questions["SimilarQuestions"] = questions["SimilarQuestions"].fillna("").apply(list)
+questions["SimilarQuestions"] = questions["SimilarQuestions"].apply(
+    lambda w: [int(q) for q in w]
+)
+questions["Code"] = questions["Code"].fillna("")
+questions["Body"] = questions["Body"].fillna("")
+questions["Companies"] = (
+    questions["Companies"]
+    .fillna(np.nan)
+    .replace([np.nan], [None])
+)
+```
+
+Create a PostgreSQL database using the [SQL](/sql/create.sql) dump and insert data using `sqlalchemy`.
+
+```python
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import dotenv_values
+
+config = dotenv_values("../.env")
+engine = create_engine(
+    f"postgresql://{config['SUPABASE_USERNAME']}:{config['SUPABASE_PASSWORD']}@{config['SUPABASE_HOSTNAME']}:{config['SUPABASE_PORT']}/{config['SUPABASE_DBNAME']}",
+    echo=True,
+)
+questions.to_sql(
+    con=engine, name="questions", if_exists="append", index=False
+)
+ls.topicTags.to_sql(
+    con=engine, name="topic_tags", if_exists="append", index=False
+)
+ls.categories.to_sql(
+    con=engine, name="categories", if_exists="append", index=False
+)
+ls.companies.to_sql(
+    con=engine, name="companies", if_exists="append", index=False
+)
+ls.questionTopics.to_sql(
+    con=engine, name="question_topics", if_exists="append", index=True, index_label="id"
+)
+ls.questionCategory.to_sql(
+    con=engine,
+    name="question_category",
+    if_exists="append",
+    index=True,
+    index_label="id",
+)
+```
+
+Using the `queried_questions_list` PostgreSQL function (defined in the SQL dump) to query for questions containy query terms:
+
+```sql
+select * from queried_questions_list('<query term>');
+```
+
+Using the `all_questions_list` PostgreSQL function (defined in the SQL dump) to query for all the questions in the database:
+
+```sql
+select * from all_questions_list();
+```
--- src/leetcode/GetQuestionsList.py
+++ src/leetcode/GetQuestionsList.py
@@ -16,11 +16,13 @@ class GetQuestionsList:
 
     def scrape_categories_and_topicTags_lists(self):
         print("Scraping Categories ... ", end="")
-        data = requests.get("https://leetcode.com/_next/data/ZLa6ykIj3i3BfoQNxcfl8/problemset/all.json").json()
+        data = requests.get(
+            "https://leetcode.com/_next/data/ZLa6ykIj3i3BfoQNxcfl8/problemset/all.json"
+        ).json()
         # List of problem categories
-        self.categories = pd.json_normalize(data["pageProps"]["problemsetCategories"]).rename(
-            columns={"title": "name", "titleSlug": "slug"}
-        )[["slug", "name"]]
+        self.categories = pd.json_normalize(
+            data["pageProps"]["problemsetCategories"]
+        ).rename(columns={"title": "name", "titleSlug": "slug"})[["slug", "name"]]
         print("Done")
         # List of problem topic tags
         print("Scraping Topic Tags ... ", end="")
@@ -35,11 +37,18 @@ class GetQuestionsList:
         for category in self.categories["slug"].values:
             data = {
                 "query": "\n    query problemsetQuestionList($categorySlug: String, $limit: Int, $skip: Int, $filters: QuestionListFilterInput) {\n  problemsetQuestionList: questionList(\n    categorySlug: $categorySlug\n    limit: $limit\n    skip: $skip\n    filters: $filters\n  ) {\n    questions: data {\n      QID: questionFrontendId\n    }\n  }\n}\n    ",
-                "variables": {"categorySlug": category, "skip": 0, "limit": self.limit, "filters": {}},
+                "variables": {
+                    "categorySlug": category,
+                    "skip": 0,
+                    "limit": self.limit,
+                    "filters": {},
+                },
             }
 
             r = requests.post("https://leetcode.com/graphql", json=data).json()
-            categories = pd.json_normalize(r["data"]["problemsetQuestionList"]["questions"])
+            categories = pd.json_normalize(
+                r["data"]["problemsetQuestionList"]["questions"]
+            )
             categories["categorySlug"] = category
             categories_data.append(categories)
         self.questionCategory = pd.concat(categories_data, axis=0, ignore_index=True)
@@ -49,14 +58,31 @@ class GetQuestionsList:
         print("Scraping questions list ... ", end="")
         data = {
             "query": "\n    query problemsetQuestionList($categorySlug: String, $limit: Int, $skip: Int, $filters: QuestionListFilterInput) {\n  problemsetQuestionList: questionList(\n    categorySlug: $categorySlug\n    limit: $limit\n    skip: $skip\n    filters: $filters\n  ) {\n    total: totalNum\n    questions: data {\n      acceptanceRate: acRate\n      difficulty\n     QID: questionFrontendId\n      paidOnly: isPaidOnly\n      title\n      titleSlug\n    topicTags {\n        slug\n      }\n    }\n  }\n}\n    ",
-            "variables": {"categorySlug": "", "skip": 0, "limit": self.limit, "filters": {}},
+            "variables": {
+                "categorySlug": "",
+                "skip": 0,
+                "limit": self.limit,
+                "filters": {},
+            },
         }
 
         r = requests.post("https://leetcode.com/graphql", json=data).json()
-        self.questions = pd.json_normalize(r["data"]["problemsetQuestionList"]["questions"])[
-            ["QID", "title", "titleSlug", "difficulty", "acceptanceRate", "paidOnly", "topicTags"]
+        self.questions = pd.json_normalize(
+            r["data"]["problemsetQuestionList"]["questions"]
+        )[
+            [
+                "QID",
+                "title",
+                "titleSlug",
+                "difficulty",
+                "acceptanceRate",
+                "paidOnly",
+                "topicTags",
+            ]
         ]
-        self.questions["topicTags"] = self.questions["topicTags"].apply(lambda w: [tag["slug"] for tag in w])
+        self.questions["topicTags"] = self.questions["topicTags"].apply(
+            lambda w: [tag["slug"] for tag in w]
+        )
         print("Done")
 
     def extract_question_topics(self):
@@ -79,13 +105,22 @@ class GetQuestionsList:
         print("Done")
 
     def add_category_to_questions_list(self):
-        self.questions["topicTags"] = self.questions["topicTags"].apply(lambda w: ",".join(w))
-        self.questions = self.questions.join(self.questionCategory.set_index("QID"), on="QID")
+        self.questions["topicTags"] = self.questions["topicTags"].apply(
+            lambda w: ",".join(w)
+        )
+        self.questions = self.questions.join(
+            self.questionCategory.set_index("QID"), on="QID"
+        )
 
-    def to_csv(self, path: str):
-        self.companies.to_csv(path + "companies.csv", index=False)
-        self.questions.to_csv(path + "questions.csv", index=False)
-        self.questionTopics.to_csv(path + "questionTopics.csv", index=True, index_label="id")
-        self.categories.to_csv(path + "categories.csv", index=False)
-        self.topicTags.to_csv(path + "topicTags.csv", index=False)
-        self.questionCategory.to_csv(path + "questionCategory.csv", index=True, index_label="id")
+    def to_csv(self, directory_path: str):
+        self.companies.to_csv(directory_path + "companies.csv", index=False)
+        self.questions["QID"] = self.questions["QID"].astype(int)
+        self.questions.to_csv(directory_path + "questions.csv", index=False)
+        self.questionTopics.to_csv(
+            directory_path + "questionTopics.csv", index=True, index_label="id"
+        )
+        self.categories.to_csv(directory_path + "categories.csv", index=False)
+        self.topicTags.to_csv(directory_path + "topicTags.csv", index=False)
+        self.questionCategory.to_csv(
+            directory_path + "questionCategory.csv", index=True, index_label="id"
+        )
